614f451b712c83d56fec8a005310a57cdc9b30ca
sharyu <sharayumarwadi11@gmail.com>
2024-03-06 20:10:15 +0530

    sort structure created

--- todo/src/components/Navbar.tsx
+++ todo/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-function Navbar({searchInp,onSearchChange}:{searchInp:string, onSearchChange: (e: any)=>void}) {
+function Navbar({ searchInp, onSearchChange }: { searchInp: string, onSearchChange: (e: any) => void }) {
+    const [dropdown,setDropdown] = useState<Boolean>(false)
     return (
 
         <nav className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 relative flex h-16 items-center">
@@ -16,6 +18,20 @@ function Navbar({searchInp,onSearchChange}:{searchInp:string, onSearchChange: (e
                     <input type="text" id="search-navbar" name="search-navbar" value={searchInp} onChange={onSearchChange} className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
                 </div>
             </div>
+            <button onClick={()=>{setDropdown(!dropdown)}} id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Dropdown <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+            </svg></button>
+            <div id="dropdownNavbar" className={`z-10 ${!dropdown ? "hidden" : ""} font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}>
+                <ul className="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                    <li>
+                        <p className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Date</p>
+                    </li>
+                    <li>
+                        <p className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Task Name</p>
+                    </li>
+                </ul>
+            </div>
+
         </nav>
     )
 }
