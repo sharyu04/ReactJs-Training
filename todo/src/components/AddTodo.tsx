de607982e097aeef1f3e994c41546e106fbd9912
sharyu <sharayumarwadi11@gmail.com>
2024-03-13 11:10:03 +0530

    Css Fixed

--- todo/src/components/AddTodo.tsx
+++ todo/src/components/AddTodo.tsx
@@ -40,9 +40,9 @@ const AddTodo = () => {
  
     return (
         <div id="addTodo" className="w-2/5 m-auto">
-            <input id="input" type="text" placeholder="Add your todo" name="input" className="p-px text-lg" value={task} onChange={handleChangeTask} />
+            <input id="input" type="text" placeholder="Add your todo" name="input" className="p-2 mr-5 rounded-2xl border-2 border-slate-400 text-lg" value={task} onChange={handleChangeTask} />
             <input type="date" placeholder="Due date" name="dueDate" id="dueDate" className="p-px text-lg" value={dueDate} onChange={handleChangeDate} />
-            <button id="addBtn" className="h-8 bg-transparent hover:bg-blue-500 text-blue-700 hover:text-white px-4 border border-blue-500 hover:border-transparent rounded m-1" onClick={btnClickHandler}>Add</button>
+            <button id="addBtn" className="h-8 mx-5 bg-transparent hover:bg-blue-500 text-blue-700 hover:text-white px-4 border border-blue-500 hover:border-transparent rounded m-1" onClick={btnClickHandler}>Add</button>
             {success ? <h3> Todo added successfully </h3> : <></>}
         </div>
     )
